34e3c18545acd2be14884899a367a75b61358030
Reyad-Mousa <99836425+Reyad-Mousa@users.noreply.github.com>
2023-11-02 20:37:47 +0200

    update Contact

--- src/app/(static)/contact/page.jsx
+++ src/app/(static)/contact/page.jsx
@@ -4,8 +4,8 @@ import Input from "postcss/lib/input";
 import style from "./page.module.css";
 function contact() {
   return (
-    <div className="mx-auto sm:p-20 flex w-auto">
-      <div className=" max-lg:hidden w-auto bg-cover">
+    <div className="mx-auto sm:p-20 flex w-auto  ">
+      <div className=" max-lg:hidden w-auto bg-cover ">
         <Image
           className={style.image}
           src="/images/contact.png"
@@ -20,67 +20,67 @@ function contact() {
           overflow: "hidden",
           width: "-webkit-fill-available",
         }}
-        className="w-auto px-12 py-10 bg-white flex-col justify-start items-center gap-10 inline-flex"
+        className="w-auto px-12 py-10   shadow-md flex-col justify-start items-center gap-10 inline-flex"
       >
-        <div className="text-center text-slate-800 text-2xl font-bold font-['Montserrat'] leading-loose tracking-tight">
-          Book Appointment
+        <div className="text-center  text-2xl font-bold font-['Montserrat'] leading-loose tracking-tight">
+          Contact To More Information
         </div>
-        <div className="w-full max-w-xs">
-          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="w-full max-w-xs ">
+          <form className=" shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <div className="mb-4">
               <label
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className="block text-sm font-bold mb-2"
                 htmlFor="username"
               >
                 Username
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-purple-700 text-black focus:shadow-outline"
                 id="username"
                 type="text"
                 placeholder="Username"
               />
             </div>
-            <div className="mb-6">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="password"
-              >
-                Password
+            <div className="mb-4">
+              <label className="block text-sm font-bold mb-2" htmlFor="Email">
+                Email
               </label>
               <input
-                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                type="password"
-                placeholder="******************"
+                className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-purple-700 text-black focus:shadow-outline"
+                id="Email"
+                type="email"
+                placeholder="example@gmail.com"
               />
-              <p className="text-red-500 text-xs italic">
-                Please choose a password.
-              </p>
             </div>
-            <div className="flex items-center justify-between">
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button"
-              >
-                Sign In
-              </button>
-              <Link
-                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                href="#"
-              >
-                Forgot Password?
-              </Link>
+            <div className="mb-4">
+              <label className="block text-sm font-bold mb-2" htmlFor="Phone">
+                Phone
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-black focus:outline-purple-700 text-black "
+                id="Phone"
+                type="number"
+                placeholder="Phone"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-bold mb-2" htmlFor="Massage">
+                Massage
+              </label>
+              <textarea
+                className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-purple-700 text-black focus:shadow-outline"
+                id="Massage"
+                type="text"
+                placeholder="Add Massage"
+              />
+            </div>
+
+            <div className=" cursor-pointer self-stretch h-12 px-10 py-3.5 bg-sky-500 rounded flex-col justify-start items-center gap-2.5 flex">
+              <div className="text-center text-white text-sm font-bold font-['Montserrat'] leading-snug tracking-tight">
+                Send
+              </div>
             </div>
           </form>
-          <p className="text-center text-gray-500 text-xs">
-            &copy;2020 Acme Corp. All rights reserved.
-          </p>
-        </div>
-        <div className="self-stretch h-12 px-10 py-3.5 bg-sky-500 rounded flex-col justify-start items-center gap-2.5 flex">
-          <div className="text-center text-white text-sm font-bold font-['Montserrat'] leading-snug tracking-tight">
-            Book Appointment
-          </div>
         </div>
       </div>
     </div>
