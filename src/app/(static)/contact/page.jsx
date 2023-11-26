358979e06585fde42a2f09c624b305b393135dfa
Reyad-Mousa <99836425+Reyad-Mousa@users.noreply.github.com>
2023-11-26 14:37:26 +0200

    change in contact page

--- src/app/(static)/contact/page.jsx
+++ src/app/(static)/contact/page.jsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
-
+import { MdOutlinePhoneCallback } from "react-icons/md";
+import { GiMoneyStack } from "react-icons/gi";
+import { IoBugOutline } from "react-icons/io5";
+import { BsJournalText } from "react-icons/bs";
 const dataForm = [
   {
     id: "Name",
@@ -23,25 +26,26 @@ const dataForm = [
 ];
 const dataContact = [
   {
-    Image: "/images/icons/phone-call.png",
+    Image: <MdOutlinePhoneCallback />,
     title: "Technical support",
     email: "support@example.com",
     number: "+1 123 456 7890",
   },
   {
-    Image: "/images/icons/money.png",
+    Image: <GiMoneyStack />,
+
     title: "Sales questions",
     email: "sales@example.com",
     number: "+1 123 456 7890",
   },
   {
-    Image: "/images/icons/press-release.png",
+    Image: <BsJournalText />,
     title: "Press",
     email: "press@example.com",
     number: "+1 123 456 7890",
   },
   {
-    Image: "/images/icons/bug.png",
+    Image: <IoBugOutline />,
     title: "Bug report",
     email: "bug@example.com",
     number: "+1 123 456 7890",
@@ -49,8 +53,8 @@ const dataContact = [
 ];
 function contact() {
   return (
-    <div className="container my-12 mx-auto ">
-      <section className="mb-32 ">
+    <div className="container mx-auto ">
+      <section>
         <div className="flex justify-center ">
           <div className="text-center md:max-w-xl lg:max-w-3xl">
             <h2 className="mb-12 px-6 text-3xl font-bold">Contact us</h2>
@@ -94,31 +98,37 @@ function contact() {
           </div>
         </div>
 
-        <div className="flex flex-wrap">
-          {dataContact.map((data) => (
-            <div
-              key={data.title}
-              className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12  flex justify-center"
-            >
-              <div className="flex items-start w-full md:w-full p-6">
-                <div className="shrink-0">
-                  <div className="inline-block rounded-md bg-teal-400-100 p-4 bg-purple-700">
-                    <Image
-                      src={data.Image}
-                      width={50}
-                      height={50}
-                      alt="Picture of the author"
-                    />
+        <div className="flex flex-wrap items-center ">
+          <div className="w-1/2 shrink-0 grow-0 basis-auto sm:flex justify-start hidden">
+            <Image
+              src="/images/contact2.png"
+              priority
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </div>
+          <div className="flex flex-wrap w-full md:w-1/2">
+            {dataContact.map((data) => (
+              <div
+                key={data.title}
+                className="mb-12 shrink-0 grow-0 basis-auto md:w-6/12  flex justify-center"
+              >
+                <div className="flex items-start w-full md:w-full  md:p-6">
+                  <div className="shrink-0">
+                    <div className="inline-block rounded-md bg-teal-400-100 p-4 text-5xl text-purple-700">
+                      {data.Image}
+                    </div>
+                  </div>
+                  <div className="ml-6 grow">
+                    <p className="mb-2 font-bold">{data.title}</p>
+                    <p className="text-neutral-500 ">{data.email}</p>
+                    <p className="text-neutral-500 ">{data.number}</p>
                   </div>
                 </div>
-                <div className="ml-6 grow">
-                  <p className="mb-2 font-bold">{data.title}</p>
-                  <p className="text-neutral-500 ">{data.email}</p>
-                  <p className="text-neutral-500 ">{data.number}</p>
-                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </section>
     </div>
