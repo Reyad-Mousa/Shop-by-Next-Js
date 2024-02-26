1b146b9ae4019652ef1af7abc438b7e7cc6d0463
Reyad-Mousa <99836425+Reyad-Mousa@users.noreply.github.com>
2024-02-26 17:14:45 +0200

    up data page details

--- src/app/(dynamic)/products/[id]/page.jsx
+++ src/app/(dynamic)/products/[id]/page.jsx
@@ -5,42 +5,64 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import { fetchProductId } from "@/app/api/fetchApi";
 import { useEffect, useState } from "react";
-import { data } from "autoprefixer";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 const Product = ({ params }) => {
   const [productId, setProductId] = useState({});
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   useEffect(() => {
-    fetchProductId(params.id).then(setProductId);
+    fetchProductId(params?.id).then(setProductId);
   }, []);
 
-  return (
-    <div className="  grid grid-cols-2  w-screen  h-screen ">
-      <div className="flex justify-center">
-        {/* {productId.images &&
-            productId.images.map((image) => ( */}
-        <Image
-          className={` ${styles.image}  hover:scale-110 m-2 h-auto rounded-xl`}
-          // key={image.length}
-          // src={image}
-          src={productId.thumbnail}
-          priority
-          width={1000}
-          height={1000}
-          alt="Picture of the author"
-        />
-        {/* // ))} */}
-      </div>
+  const handleNextImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) => (prevIndex + 1) % productId.images.length
+    );
+  };
 
-      <div className="">
-        <h3 className=" pb-16  font-bold text-2xl uppercase">
-          {productId.title}
-        </h3>
-        <h1 className=" font-bold">Description :</h1>
+  const handlePrevImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) =>
+        (prevIndex - 1 + productId.images.length) % productId.images.length
+    );
+  };
+  return (
+    <div className="  sm:grid sm:grid-cols-2 sm:px-3 h-screen gap-5  ">
+      <div className="flex justify-between items-center h-60 p-5  ">
+        <FaAngleLeft
+          className=" mr-3 bg-white h-20 text-black rounded-xl w-6 cursor-pointer"
+          onClick={handlePrevImage}
+        />{" "}
+        <div className="flex   ">
+          {productId?.images?.map((image, index) => (
+            <Image
+              className={` 
+              h-48 w-48 max-w-48 sm:h-72 sm:w-72 md:w-full
+                 rounded-xl ${
+                index === currentImageIndex ? "block" : "hidden"
+              }`}
+              key={image.length}
+              src={image}
+              priority
+              width={400}
+              height={400}
+              alt="Picture of the author"
+            />
+          ))}
+        </div>
+        <FaAngleRight
+          className=" ml-3   bg-white text-black h-20 rounded-xl w-6 cursor-pointer"
+          onClick={handleNextImage}
+        />
+      </div>
+      <div className=" gap-2 flex flex-col">
+        <h3 className="   font-bold text-2xl uppercase">{productId.title}</h3>
+        <h1 className=" font-bold">Description : {productId.description}</h1>
         <p className=" mt-1  overflow-ellipsis overflow-clip">{productId.id}</p>
         <h1 className="font-bold">
           Price :
-          <span className="font-bold  text-red-600"> {productId.title}</span>
+          <span className="font-bold  text-red-600"> {productId.price}</span>
         </h1>
         <Link
           className="mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
