ba245983c7abbb0f812ea99fac906336dc6e425c
Reyad-Mousa <99836425+Reyad-Mousa@users.noreply.github.com>
2024-02-27 20:21:58 +0200

    Update page.jsx

--- src/app/(dynamic)/products/[id]/page.jsx
+++ src/app/(dynamic)/products/[id]/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 import Link from "next/link";
 import { fetchProductId } from "@/app/api/fetchApi";
 import { useEffect, useState } from "react";
